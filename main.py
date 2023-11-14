504a2be7e0b34015aaffa7ad2ae7a11df51aa1fd
Simone Rizzo <33527032+simone-rizzo@users.noreply.github.com>
2023-11-14 15:54:47 +0100

    Update main.py
    aggiornamento:
    - range di lavoro
    - assenze

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
+import datetime
 import openpyxl
 from openpyxl.styles import PatternFill
 import re
 import random
 import streamlit as st
+
+class CalculusErrorException(Exception):
+    "Raised when the the working our can't fit in the range with the max hours per day."
+    pass
+
+class StartEndDateException(Exception):
+    "Raised when the start date or end date are not in the days list, maybe because they are festival days."
+    pass
+
+class SaveFileException(Exception):
+    "Raised when there is an error while saving the file."
+    pass
+
 
 def get_cell_color(workbook, cell_coordinates):
     sheet = workbook.active
@@ -12,6 +26,10 @@ def get_cell_color(workbook, cell_coordinates):
 def set_cell_value(workbook, cell_coordinates, value):
     sheet = workbook.active
     sheet[cell_coordinates] = value
+
+def get_cell_value(workbook, cell_coordinates):
+    sheet = workbook.active
+    return sheet[cell_coordinates].value
 
 def set_cell_color(workbook, cell_coordinates, color_hex):
     sheet = workbook.active
@@ -69,12 +87,19 @@ def split_alpha_numeric(s):
 
 def get_all_days(workbook):
     days = []
-    for month in list(row_maps.keys()):
-        month_cells = get_cells_between(f"{columns[0]}{month}",f"{columns[1]}{month}")
+    date_cells_hash = {}
+    for i, month_row in enumerate(list(row_maps.keys())):
+        month_cells = get_cells_between(f"{columns[0]}{month_row}",f"{columns[1]}{month_row}")
         for d in month_cells:
             if get_cell_color(workbook, d) == WHITE_COLOR:
+                mese = i+1
+                lett, num = split_alpha_numeric(d)
+                giorno = get_cell_value(workbook, f"{lett}{int(num)-1}")
+                data = f"{giorno}/{mese}"
+                # controllare se la data è inclusa nel range.
                 days.append(d)
-    return days
+                date_cells_hash[data] = d
+    return days, date_cells_hash
 
 def set_value(workbook, coord, value):
     if value > 0:
@@ -121,9 +146,32 @@ def save_uploaded_file(uploaded_file):
             f.write(uploaded_file.getbuffer())
         return uploaded_file.name
     except Exception as e:
-        return None
-    
-def start_function(file_path, nome, cognome, n_ore, max_per_day):
+        raise SaveFileException() from e
+
+def filter_by_start_end_date(days, date_cells_hash, start_date, end_date):
+    start = f"{start_date.day}/{start_date.month}"
+    end = f"{end_date.day}/{end_date.month}"
+    try:
+        lista_chiavi = list(date_cells_hash.keys())
+        start_index = lista_chiavi.index(start)
+        end_index = lista_chiavi.index(end)
+        return days[start_index:end_index]
+    except Exception as e:
+        raise StartEndDateException() from e
+
+def filter_by_absences(workbook, days, date_cells_hash):
+    for abs in st.session_state['date_list']:
+        splitted = abs.split("/")
+        label = splitted[-1]
+        hash_date = str(int(splitted[0].split("-")[-1]))+"/"+str(int(splitted[0].split("-")[-2]))
+        set_cell_value(workbook, date_cells_hash[hash_date], label)
+        # remove from the list that hash
+        days.remove(date_cells_hash[hash_date])
+        date_cells_hash.pop(hash_date)
+    return days, date_cells_hash
+        
+
+def start_function(file_path, nome, cognome, n_ore, max_per_day, start_date, end_date):
     
     # Carica il workbook
     workbook = openpyxl.load_workbook(file_path)
@@ -135,11 +183,16 @@ def start_function(file_path, nome, cognome, n_ore, max_per_day):
     set_name(sheet, nome, cognome)
 
     # Get days
-    days = get_all_days(workbook)
+    days, date_cells_hash = get_all_days(workbook)
+
+    days, date_cells_hash = filter_by_absences(workbook, days, date_cells_hash)
+
+    days = filter_by_start_end_date(days, date_cells_hash, start_date, end_date)
+    # filtra con data inizio e fine
 
     if len(days) * max_per_day < n_ore:
         # print(f"Le ore totali inserite superano le 4 ore giornaliere massime per i {len(days)} disponibili.")
-        return None
+        raise CalculusErrorException()
 
     distribute_hours(workbook, n_ore, max_per_day, days)
 
@@ -148,29 +201,66 @@ def start_function(file_path, nome, cognome, n_ore, max_per_day):
     workbook.save(filename)
     return filename
 
+# Inizializza una sessione per tenere traccia delle date aggiunte
+if 'date_list' not in st.session_state:
+    st.session_state['date_list'] = []
+
+# Funzione per aggiungere date all'elenco
+def add_date():
+    date_str = f"{selected_date}/{selected_label}"
+    if date_str not in st.session_state['date_list']:
+        st.session_state['date_list'].append(date_str)
+
+# Funzione per rimuovere date dall'elenco
+def remove_date(date_str):
+    st.session_state['date_list'].remove(date_str)
+
 if __name__ == "__main__":
+    # workbook = openpyxl.load_workbook("FOGLIO PRESENZA 2021 R&S VUOTO.xlsx")
+    # get_all_days(workbook=workbook)
     st.set_page_config(page_title="Compilatore", page_icon="📄", layout="centered")
     st.title('Compilatore foglio di Presenza')
-    with st.form("my_form"):
-        nome = st.text_input('Nome')
-        cognome = st.text_input('Cognome')
-        total_hours = st.number_input('Totale Ore', min_value=0)
-        max_per_day = st.number_input('Max ore per giorno', min_value=0)
-        uploaded_file = st.file_uploader("Foglio di presenza Excel vuoto", type="xlsx")
-        submitted = st.form_submit_button("Compila foglio di presenza")
+    nome = st.text_input('Nome')
+    cognome = st.text_input('Cognome')
+    total_hours = st.number_input('Totale Ore', min_value=0)
+    max_per_day = st.number_input('Max ore per giorno', min_value=0)
+    start_date = st.date_input("Data inizio")
+    end_date = st.date_input("Data fine")
+
+    st.subheader("Inserisci Date assenze:")
+    # Seleziona la data e la label
+    selected_date = st.date_input("Seleziona una data")
+    selected_label = st.selectbox("Seleziona una label", ["F", "P", "M", "A"])
+
+    # Pulsante per aggiungere la data
+    st.button("Aggiungi Data", on_click=add_date)
+
+    # Mostra l'elenco delle date aggiunte
+    st.subheader("Date assenze Selezionate:")
+    for date in st.session_state['date_list']:
+        col1, col2 = st.columns([0.8, 0.2])
+        col1.write(date)
+        col2.button("Rimuovi", key=date, on_click=remove_date, args=(date,))
+    
+    uploaded_file = st.file_uploader("Foglio di presenza Excel vuoto", type="xlsx")
+    submitted = st.button("Compila foglio di presenza")
 
     if submitted and uploaded_file:
-        file_path = save_uploaded_file(uploaded_file)
-        if file_path:
-            output_file_path = start_function(file_path, nome, cognome, total_hours, max_per_day)
+        try:
+            file_path = save_uploaded_file(uploaded_file)
+            output_file_path = start_function(file_path, nome, cognome, total_hours, max_per_day, start_date, end_date)
             if output_file_path:
                 with open("./"+output_file_path, "rb") as file:
                     st.download_button(label='📥 Scarica Excel',
                                     data=file,
                                     file_name=output_file_path,
                                     mime='application/vnd.ms-excel')
-            else:
-                st.text(f"Il Totale ore supera il massimo di numero di ore lavorabili con {max_per_day} ore giornaliere.")
-        else:
-            st.error("Errore nel salvataggio del file.")
+        except CalculusErrorException:
+            st.text(f"Il Totale ore supera il massimo di numero di ore lavorabili con {max_per_day} ore giornaliere, per il range di giorni selezionati.")
+        except StartEndDateException:
+            st.text("Data di inizio o fine non corretti. Potrebbero essere giorni festivi controlla meglio.")
+        except SaveFileException:
+            st.text("Errore nell'upload e salvataggio del file excell.")
+        except Exception as e:
+            st.error(e)
     
